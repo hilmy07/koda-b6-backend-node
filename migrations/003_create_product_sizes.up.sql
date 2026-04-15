30c0bdd2db23e7a4fd7b641ce4c8b3f19d535cf6
mrrcodex <mrramadhandev@gmail.com>
2026-04-15 11:46:55 +0700

    feat: add migration product_sizes table

--- migrations/003_create_product_sizes.up.sql
+++ migrations/003_create_product_sizes.up.sql
@@ -5,8 +5,5 @@ CREATE TABLE product_sizes (
     CONSTRAINT product_sizes_product_id_fkey
         FOREIGN KEY (product_id)
         REFERENCES products(id)
-        ON DELETE CASCADE,
-    CONSTRAINT product_sizes_size_id_fkey
-        FOREIGN KEY (size_id)
-        REFERENCES sizes(id)
+        ON DELETE CASCADE
 );
